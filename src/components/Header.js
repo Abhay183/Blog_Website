2e21917834ddcdffbc9a8c03480eeb430d38ac5e
unknown <abgupta2003@gmail.com>
2024-12-10 15:15:52 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Header=({label})=>{
+    return(
+        <div className="text-bg-primary p-3 text-center fs-3">
+          {label}
+        </div>
+    );
+}
+
+export default Header;
